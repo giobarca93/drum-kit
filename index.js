15fb5685d817fecf9f1a8ba746e54f17fb718411
Giovani Minchola <wgmin1@student.monash.edu>
2019-10-25 18:02:30 +1100

    last changes

--- index.js
+++ index.js
@@ -68,5 +68,8 @@ function buttonAnimation(currentkey){
     var activeButton = document.querySelector("." + currentkey);
     activeButton.classList.add("pressed");
 
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 };
 
